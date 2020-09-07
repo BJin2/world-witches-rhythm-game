c2f0b7ef6884923b3399866ced078fd43f7c44fb
Jin <bomjan@naver.com>
2020-09-07 23:19:39 +0900

    Created skeleton classes
    - Frame for spawning neurois
    - Neuroi base class
    - Spawner class
    - Hit position class

---
+++ Assets/Scripts/HitLaserTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitLaserTrigger : MonoBehaviour
+{
+	private void OnTriggerEnter(Collider col)
+	{
+		//make the colliding neuroi shoot laser
+	}
+
+	private void OnTriggerExit(Collider col)
+	{
+		//start decreasing the neuroi's score
+	}
+}
---
+++ Assets/Scripts/Neuroi.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Neuroi : MonoBehaviour
+{
+	//big neuroi and small neuroi classes will inherit this abstract class
+}
