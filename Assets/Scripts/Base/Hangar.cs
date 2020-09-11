5990ffda6b801561c7ece2f8c29c20a7e114445f
Jin <bomjan@naver.com>
2020-09-11 16:01:39 +0900

    Flight formation implemented

--- Assets/Scripts/Base/Hangar.cs
+++ Assets/Scripts/Base/Hangar.cs
@@ -1,14 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
 
 public class Hangar : KeyAction
 {
 	[SerializeField]
 	private GameObject hangarUI = null;
 	[SerializeField]
+	private GameObject characterUI = null;
+	[SerializeField]
 	private List<MonoBehaviour> listToDiable = null;
+	private Dictionary<string, Sprite> profiles = null;
 
+	private void Start()
+	{
+		profiles = new Dictionary<string, Sprite>();
+		foreach (string name in CharacterInfo.characterName)
+		{
+			profiles.Add(name, Resources.Load("Sprites/" + name, typeof(Sprite)) as Sprite);
+		}
+	}
 
 	protected override void OnTriggerEnter(Collider col)
 	{
@@ -26,41 +39,64 @@ public class Hangar : KeyAction
 		UIEnable(true);
 	}
 
+	public void TurnOffFormation()
+	{
+		UIEnable(false);
+	}
+
 	private void UIEnable(bool enable)
 	{
 		//Diable other scripts that processes input
 		foreach (MonoBehaviour toDiable in listToDiable)
 		{
-			toDiable.enabled = enable;
+			toDiable.enabled = !enable;
 		}
 
 		//Turn on hangar UI
 		hangarUI.SetActive(enable);
-		InitializeFormation();
+		CharacterInfo.Init();
+		for (int i = 0; i < 5; i++)
+		{
+			UpdateProfile(i);
+		}
 	}
 
-	private void InitializeFormation()
-	{
-		//Clear flight member slots
-	}
-
-	#region Functions for member selection
+#region Functions for member selection
 
 	public void ConfirmMember()
 	{
 		//Check all slots are filled
-
+		foreach (string member in CharacterInfo.flightMember)
+		{
+			if (member == "")
+				return;
+		}
 		AsyncSceneLoader.LoadAsyncAdditive("Gameplay", this);
+	}
+
+	public void SelectMember(int index)
+	{
+		CharacterInfo.currentMemberIndex = index;
+		characterUI.SetActive(true);
 	}
 
 	public void SelectCharacter(string name)
 	{
-		//Check if the character is already selected
-		//Then swap
+		//Current slot is already occupied or selected character already exist
+		for (int i = 0; i < CharacterInfo.flightMember.Count; i++)
+		{
+			if (CharacterInfo.flightMember[i] == name)
+			{
+				//Swap
+				CharacterInfo.flightMember[i] = CharacterInfo.flightMember[CharacterInfo.currentMemberIndex];
+				UpdateProfile(i);
+				break;
+			}
+		}
 
-		//Otherwise set it to member slot
+		CharacterInfo.flightMember[CharacterInfo.currentMemberIndex] = name;
+		UpdateProfile(CharacterInfo.currentMemberIndex);
 
-		//If empty string, empty the slot
 		//Close Character selection window
 		GameObject.Find("CharacterList").SetActive(false);
 	}
@@ -68,6 +104,22 @@ public class Hangar : KeyAction
 	public void CloseWindow(GameObject toClose)
 	{
 		toClose.SetActive(false);
+	}
+
+	private void UpdateProfile(int index)
+	{
+		string name = CharacterInfo.flightMember[index];
+		Transform slotBG = hangarUI.transform.Find("BG").Find("SlotBG");
+		Image profile = slotBG.GetChild(index).Find("Profile").GetComponent<Image>();
+		if (name != "")
+		{
+			profile.sprite = profiles[CharacterInfo.flightMember[index]];
+			profile.color = new Color(255, 255, 255, 1.0f);
+		}
+		else
+		{
+			profile.color = new Color(255, 255, 255, 0.0f);
+		}
 	}
 
 #endregion
