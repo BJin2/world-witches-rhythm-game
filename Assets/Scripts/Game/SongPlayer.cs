8adf0dbcbda4b28d384b0cefbc0a9df1d19b69a5
Jin <bomjan@naver.com>
2020-09-21 21:25:21 +0900

    Playing music part by part
    - Playing music part by part based on selected characters

--- Assets/Scripts/Game/SongPlayer.cs
+++ Assets/Scripts/Game/SongPlayer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SongPlayer : MonoBehaviour
@@ -7,9 +8,11 @@ public class SongPlayer : MonoBehaviour
 	public static string songName = "Bookmark A Head";
 	private List<AudioSource> audioSource = null;
 	private SongInfo info = null;
+	private int partIndex = 0;
 	private AudioClip[] song = null;
 	[SerializeField]
 	private float delay = 3.0f;
+	private float timer = 0.0f;
 
 	private void Awake()
 	{
@@ -28,10 +31,33 @@ public class SongPlayer : MonoBehaviour
 		//TODO start playing after delay second
 	}
 
+	private void Update()
+	{
+		timer += Time.deltaTime;
+		if (timer >= delay)
+		{
+			Play();
+		}
+	}
+
 	public void Play()
 	{
 		//Read info.part and change the one playing audio
 		//Access each Character with Flight.flight and info.part[?].part
+		if (partIndex >= info.part.Count)
+			return;
+
+		if (partIndex > 0)
+			audioSource[info.part[partIndex - 1].singer].Stop();
+
+		//audioSource[info.part[partIndex].singer].PlayScheduled(info.part[partIndex].timing);
+		audioSource[info.part[partIndex].singer].time = info.part[partIndex].timing;
+		audioSource[info.part[partIndex].singer].Play();
+
+		//Prepare for next;
+		partIndex++;
+		if(partIndex < info.part.Count)
+			delay = info.part[partIndex].timing;
 	}
 
 	public void AddAudio(AudioSource aud)
