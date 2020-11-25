84888b7d328742d909dad991a740623f5664ef7e
Jin <bomjan@naver.com>
2020-11-25 18:13:04 +0900

    Implemented neuroi manager gizmo

--- Assets/Scripts/Game/NeuroiManager.cs
+++ Assets/Scripts/Game/NeuroiManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class NeuroiManager : MonoBehaviour
+public partial class NeuroiManager : MonoBehaviour
 {
 	public static NeuroiManager Instance { get; private set; }
 
