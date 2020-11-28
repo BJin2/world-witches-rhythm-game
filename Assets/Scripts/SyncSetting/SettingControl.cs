2d2d228d46de0d03cfae675e66d0d3271e8daa35
Jin <bomjan@naver.com>
2020-11-29 01:48:09 +0900

    Adjusting audio&graphic sync implemented
    - User can modify time offset for neuroi spawn time
    - User can modify neuroi move speed(does not affect visual sync)

--- Assets/Scripts/SyncSetting/SettingControl.cs
+++ Assets/Scripts/SyncSetting/SettingControl.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class SettingControl : MonoBehaviour
@@ -11,6 +10,12 @@ public class SettingControl : MonoBehaviour
 	GameplayInput gInput = null;
 
 	private float[] range;
+
+	private float timer = 0.0f;
+	private float delay = float.MaxValue;
+	private float syncOffset = 0.0f;
+
+	private AudioSource aud = null;
 
 	private void Awake()
 	{
@@ -26,15 +31,45 @@ public class SettingControl : MonoBehaviour
 		{
 			range[i] = transform.Find($"Range{i}").position.z;
 		}
+
+		aud = gameObject.GetComponent<AudioSource>();
+	}
+
+	private void Start()
+	{
+		ResetNeuroi();
 	}
 
 	private void Update()
 	{
+		if(!testingNeuroi.gameObject.activeInHierarchy)
+			timer += Time.deltaTime;
+
+		if (timer >= delay)
+		{
+			testingNeuroi.gameObject.SetActive(true);
+			timer = 0.0f;
+		}
+
 		if (testingNeuroi.transform.position.z < range[1])
 		{
-			testingNeuroi.transform.position = new Vector3(0, 0, range[0]);
+			ResetNeuroi();
 		}
 		gInput.processGameplayInput();
+	}
+	private void ResetNeuroi()
+	{
+		timer = 0.0f;
+		testingNeuroi.transform.position = new Vector3(0, 0, range[0]);
+		testingNeuroi.gameObject.SetActive(false);
+		CalculateDelay();
+		aud.time = 0.0f;
+		aud.Play();
+	}
+	private void CalculateDelay()
+	{
+		float offset = (range[0] - HitRange.Instance.transform.position.z) / Neuroi.Speed;
+		delay = aud.clip.length - offset + syncOffset;
 	}
 
 	private void TouchInput()
@@ -45,11 +80,11 @@ public class SettingControl : MonoBehaviour
 			Touch t = Input.GetTouch(0);
 			if (t.phase == TouchPhase.Began)
 			{
-				testingNeuroi.ShootDown();
+				Shoot();
+				return;
 			}
 		}
 	}
-
 	private void KeyboardInput()
 	{
 		//For keyboard input
@@ -60,14 +95,61 @@ public class SettingControl : MonoBehaviour
 			{
 				if (gInput.AssignedKeyPresssed(pressed.Dequeue()))
 				{
-					if (testingNeuroi.ShootDown())
-					{
-						testingNeuroi.gameObject.SetActive(true);
-						testingNeuroi.transform.position = new Vector3(0, 0, range[0]);
-					}
+					Shoot();
 					return;
 				}
 			}
 		}
 	}
+
+	private void Shoot()
+	{
+		if (testingNeuroi.ShootDown())
+		{
+			ResetNeuroi();
+		}
+	}
+
+	public void ChangeSync(float amt)
+	{
+		syncOffset += amt;
+		Mathf.Clamp(syncOffset, -5, 5);
+	}
+	public void ChangeSpeed(float amt)
+	{
+		Neuroi.Speed += amt;
+		Mathf.Clamp(Neuroi.Speed, 1, 100);
+	}
+
+	#region Flow Control
+	public void Pause()
+	{
+		Time.timeScale = 0.0f;
+		PauseDelay.Instance.DarkBG(true);
+		PauseDelay.Instance.AfterDelay += () => { Time.timeScale = 1.0f; PauseDelay.Instance.DarkBG(false); };
+		SelectionWindow.Instance.Show("Exit Setting?", "", new List<SelectionWindow.ButtonInfo>
+		{
+			new SelectionWindow.ButtonInfo("Save&Exit", ()=>{ Save(); Exit(); }),
+			new SelectionWindow.ButtonInfo("Cancel", UnPause),
+			new SelectionWindow.ButtonInfo("Exit w/o Save", Exit)
+		});
+	}
+	private void UnPause()
+	{
+		//Prevent pausedelay to run several times over and over
+		if (Time.timeScale != 0.0f)
+			return;
+
+		PauseDelay.Instance.Delay(3.0f);
+	}
+	private void Save()
+	{
+		//TODO save sync and neuroi speed
+	}
+	private void Exit()
+	{
+		AsyncSceneLoader.LoadAsyncAdditive("Base", this); 
+		Time.timeScale = 1.0f;
+	}
+	#endregion
 }
